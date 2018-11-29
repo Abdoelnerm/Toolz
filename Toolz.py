8cea5e51b35d2bdaf27e7010eef316e27326c997
Abdallah X1L <34988730+Abdoelnerm@users.noreply.github.com>
2018-11-30 01:21:29 +0200

    Add files via upload

---
+++ Toolz.py
@@ -0,0 +1,145 @@
+#!/data/data/com.termux/files/usr/bin/python
+#--------------------------------------------------------------------#
+# AUTHOR:- ABDALLAH X1L ---------------------------------------------#
+# ABOUT AUTHOR:- ----------------------------------------------------#
+# NAME: Abdallah Hussein Mahmoud ------------------------------------#
+# OLD: 15 Years OLD -------------------------------------------------#
+# FROM: Egypt -------------------------------------------------------#
+# LIVE IN: Egypt, Beni Suef -----------------------------------------#
+# FACEBOOK MAIN ACCOUNT: https://www.facebook.com/100016180543506----#
+#--------------------------------------------------------------------#
+import os
+import sys
+print ("""
+▀▀█▀▀ ▒█▀▀▀█ ▒█▀▀▀█ ▒█░░░ ▒█▀▀▀█
+░▒█░░ ▒█░░▒█ ▒█░░▒█ ▒█░░░ ░▄▄▄▀▀
+░▒█░░ ▒█▄▄▄█ ▒█▄▄▄█ ▒█▄▄█ ▒█▄▄▄█
+""")
+print ("A python script to install many tools")
+print ("{1}======> To install Spamming Tools")
+print ("{2}======> To install Wordlists Tools")
+print ("{3}======> To install Best Tools")
+print ("{4}======> To install Hack Accounts Tools")
+print ("{5}======> To install Phishing Tools")
+print ("{6}======> To install Termux OS")
+user_input = input(" Enter Number: ")
+x = input("Enter Tool: ")
+
+if user_input == "1":
+   print ("""
+▒█▀▀▀█ █▀▀█ █▀▀█ █▀▄▀█ █▀▄▀█ ░▀░ █▀▀▄ █▀▀▀ 　 ▀▀█▀▀ █▀▀█ █▀▀█ █░░ █▀▀
+░▀▀▀▄▄ █░░█ █▄▄█ █░▀░█ █░▀░█ ▀█▀ █░░█ █░▀█ 　 ░▒█░░ █░░█ █░░█ █░░ ▀▀█
+▒█▄▄▄█ █▀▀▀ ▀░░▀ ▀░░░▀ ▀░░░▀ ▀▀▀ ▀░░▀ ▀▀▀▀ 　 ░▒█░░ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
+""")
+   print (" Enter Any Tool Name OF this Tools To Install IT ")
+   print (" Tools: ")
+   print (" Spammer-Grab, LiteSpam, Spame-Scope, dj-spammer, Email-Bomber")
+   if x == "spammer-grab":
+        os.system("git clone https://github.com/Abdoelnerm/Spammer-Grab-X1L")
+   elif x == "litespam":
+          os.system(" git clone https://github.com/4L13199/LITESPAM ")
+   elif x == "spam-scope":
+          os.system(" git clone https://github.com/SpamScope/spamscope ")
+   elif x == "dj-spammer":
+          os.system(" git clone https://github.com/pydanny/dj-spam ")
+   elif x == "email-bomber":
+          os.system(" git clone https://github.com/zanyarjamal/Email-bomber ")
+
+elif user_input == "2":
+   print ("""
+▒█░░▒█ █▀▀█ █▀▀█ █▀▀▄ █░░ ░▀░ █▀▀ ▀▀█▀▀ █▀▀ 　 ▀▀█▀▀ █▀▀█ █▀▀█ █░░ █▀▀
+▒█▒█▒█ █░░█ █▄▄▀ █░░█ █░░ ▀█▀ ▀▀█ ░░█░░ ▀▀█ 　 ░▒█░░ █░░█ █░░█ █░░ ▀▀█
+▒█▄▀▄█ ▀▀▀▀ ▀░▀▀ ▀▀▀░ ▀▀▀ ▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀ 　 ░▒█░░ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
+""")
+   print (" Now One Tool Is Available ")
+   print (" To install it type cupp ")
+   if x =="cupp":
+        os.system(" git clone https://github.com/Mebus/cupp ")
+
+elif user_input == "3":
+   print ("""
+▒█▀▀█ █▀▀ █▀▀ ▀▀█▀▀ 　 ▀▀█▀▀ █▀▀█ █▀▀█ █░░ █▀▀
+▒█▀▀▄ █▀▀ ▀▀█ ░░█░░ 　 ░▒█░░ █░░█ █░░█ █░░ ▀▀█
+▒█▄▄█ ▀▀▀ ▀▀▀ ░░▀░░ 　 ░▒█░░ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
+""")
+   print (" Enter Any Tool Name OF this Tools To Install IT ")
+   print (" Tools: ")
+   print (" apktool, metasploit-framework, wifite, Easy_Hack, Optiva-Framework, beef-framework, routersploit, setoolkit, tmux-bunch ")
+   if x == "metasploit-framework":
+        os.system(" git clone https://github.com/rapid7/metasploit-framework ")
+   elif x == "wifite":
+          os.system(" git clone https://github.com/derv82/wifite ")
+   elif x == "easy_hack":
+          os.system(" git clone https://github.com/sabri-zaki/EasY_HaCk ")
+   elif x == "optiva-framework":
+          os.system(" git clone https://github.com/joker25000/Optiva-Framework ")
+   elif x == "beef":
+          os.system(" git clone https://github.com/beefproject/beef ")
+   elif x == "routersploit":
+          os.system(" git clone https://github.com/threat9/routersploit ")
+   elif x == "apkinjector":
+          os.system(" git clone https://github.com/jbreed/apkinjector ")
+   elif x == "tmux-bunch":
+          os.system(" git clone https://github.com/Hax4us/Tmux-Bunch ")
+   elif x == "setoolkit":
+          os.system(" git clone https://github.com/Hax4us/setoolkit ")
+
+elif user_input == "4":
+   print ("""
+▒█░▒█ █▀▀█ █▀▀ █░█ 　 ░█▀▀█ █▀▀ █▀▀ █▀▀█ █░░█ █▀▀▄ ▀▀█▀▀ █▀▀
+▒█▀▀█ █▄▄█ █░░ █▀▄ 　 ▒█▄▄█ █░░ █░░ █░░█ █░░█ █░░█ ░░█░░ ▀▀█
+▒█░▒█ ▀░░▀ ▀▀▀ ▀░▀ 　 ▒█░▒█ ▀▀▀ ▀▀▀ ▀▀▀▀ ░▀▀▀ ▀░░▀ ░░▀░░ ▀▀▀
+▀▀█▀▀ █▀▀█ █▀▀█ █░░ █▀▀
+░▒█░░ █░░█ █░░█ █░░ ▀▀█
+░▒█░░ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
+""")
+   print (" Enter Any Tool Name OF this Tools To Install IT ")
+   print (" Tools: ")
+   print (" fb-brute, fb.py, Facebook.pl, hack-yahoo-scrapper, hydra ")
+   if x == "fb-brute":
+        os.system(" git clone https://github.com/HackerAdana/facebook-brute-force ")
+   elif x == "fb.py":
+          os.system(" git clone https://github.com/emre/fb.py ")
+   elif x == "facebook.pl":
+          os.system(" git clone https://github.com/bcattaneo/Facebook.pl ")
+   elif x == "hack-yahoo-scrapper":
+          os.system(" git clone https://github.com/kkruecke/hack-yahoo-finance-scrapper ")
+   elif x == "hydra":
+          os.system(" apt update && apt upgrade && apt -y install hydra ")
+
+elif user_input == "5":
+   print ("""
+▒█▀▀█ █░░█ ░▀░ █▀▀ █░░█ ░▀░ █▀▀▄ █▀▀▀ 　 ▀▀█▀▀ █▀▀█ █▀▀█ █░░ █▀▀
+▒█▄▄█ █▀▀█ ▀█▀ ▀▀█ █▀▀█ ▀█▀ █░░█ █░▀█ 　 ░▒█░░ █░░█ █░░█ █░░ ▀▀█
+▒█░░░ ▀░░▀ ▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀ ▀░░▀ ▀▀▀▀ 　 ░▒█░░ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
+""")
+   print (" Enter Any Tool Name OF this Tools To Install IT ")
+   print (" Tools: ")
+   print (" weeman, SocialFish, FB-Phishing, shellphish ")
+   if x == "weeman":
+        os.system(" git clone https://github.com/evait-security/weeman ")
+   elif x == "socialfish":
+          os.system(" git clone https://github.com/UndeadSec/SocialFish ")
+   elif x == "fb-phishing":
+          os.system(" git clone https://github.com/SheehabX/FB-Phishing ")
+   elif x == "shellphish":
+          os.system(" git clone https://github.com/shellphish ")
+
+elif user_input == "6":
+   print ("""
+▀▀█▀▀ █▀▀ █▀▀█ █▀▄▀█ █░░█ █░█ 　 ▒█▀▀▀█ ▒█▀▀▀█
+░▒█░░ █▀▀ █▄▄▀ █░▀░█ █░░█ ▄▀▄ 　 ▒█░░▒█ ░▀▀▀▄▄
+░▒█░░ ▀▀▀ ▀░▀▀ ▀░░░▀ ░▀▀▀ ▀░▀ 　 ▒█▄▄▄█ ▒█▄▄▄█
+""")
+   print (" Enter Any OS Name OF this OSs To Install IT ON Your Termux ")
+   print (" OS: ")
+   print (" Ubuntu, Kali-Net-Hunter, Fedora ")
+   if x == "ubuntu":
+        os.system(" git clone https://github.com/Neo-Oli/termux-ubuntu ")
+   elif x == "fedora":
+          os.system(" https://github.com/nmilosev/termux-fedora ")
+   elif x == "kalinethunter":
+          os.system(" git clone https://github.com/Hax4us/Nethunter-In-Termux ")
+
+else:
+    sys.exit()
